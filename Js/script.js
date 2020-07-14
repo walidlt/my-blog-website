11d7f52626b18c7c4e3c1a61009b1602c121e800
walidlt <ltwalid44@gmail.com>
2020-07-14 04:13:38 +0200

    up

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("body").niceScroll({
+
+        cursorcolor: "#15D5A4",
+        zindex: "9999",
+        cursorborder: "none",
+        zindex: "9999",
+        cursorwidth: "8px",
+    });
+
+
+});
